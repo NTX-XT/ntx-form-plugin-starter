6f3a78947c009f92347e9a6cc05d7cfab7976d7a
John Pouliezos <jpouliezos@gmail.com>
2023-01-30 18:50:22 +0000

    Yet more attempts to register

--- dist/nac-card.js
+++ dist/nac-card.js
@@ -1,9 +1,8 @@
 import { html, LitElement } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
-import { config } from './nac-cards.config'
 
 @customElement('nac-card')
-export default class NACCard extends LitElement {
+export class NACCard extends LitElement {
     @property({ type: String })
     title = "This is a card";
 
@@ -19,8 +18,42 @@ export default class NACCard extends LitElement {
     @property({ type: String })
     link = "https://";
 
-    static getMetaConfig() {
-        return config;
+    static getMetaConfig(): Promise<NintexPlugin> | NintexPlugin {
+        return {
+            controlName: 'Card',
+            fallbackDisableSubmit: false,
+            description: 'Example of a card with dynamic data',
+            groupName: 'Visual',
+            version: '1.0',
+            properties: {
+                title: {
+                    type: 'string',
+                    title: 'The card title',
+                    required: true
+                },
+                subtitle: {
+                    type: 'string',
+                    title: 'The card subtitle',
+                    required: false
+                },
+                text: {
+                    type: 'string',
+                    title: 'Any text for the body of the card',
+                    required: false
+                },
+                imageUrl: {
+                    type: 'string',
+                    title: 'the Url to an image',
+                    required: false
+                }
+            },
+            standardProperties: {
+                fieldLabel: true,
+                description: true,
+                defaultValue: false,
+                readOnly: false,
+            },
+        };
     }
 
     render() {
@@ -38,6 +71,3 @@ export default class NACCard extends LitElement {
         super();
     }
 }
-
-const elementName = 'nac-card';
-customElements.define(elementName, NACCard);
