7ade047469332eab305773605c08ca7d91bb88e5
John Pouliezos <jpouliezos@gmail.com>
2023-02-01 08:55:30 +0000

    Bootstrap links

--- my-plugins/my-card.js
+++ my-plugins/my-card.js
@@ -44,14 +44,14 @@ export default class MyCard extends LitElement {
 
     render() {
         return html`
-            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-            <div class="card text-white bg-dark mb-3">
-                <div class="header">${this.title}</div>
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+            <div class="card text-white bg-dark mb-3" style="width: 18rem;">
+                <div class="card-header">${this.title}</div>
                 <img src="${this.imageUrl}" class="card-img-top" alt="Plugin!"/>
                 <div class="card-body">
                     <p class="card-text">${this.text}</p>
-                    <a href="${this.link}" class="btn btn-primary">Link</a>
+                    <a href="${this.link}" class="btn btn-primary">Go!</a>
                 </div>
             </div>`;
     }
