f1109c422e9019a38ede84da650b3500d0e9ec1a
John Pouliezos <jpouliezos@gmail.com>
2023-03-30 12:42:15 +0300

    Added linting

--- rollup.config.js
+++ rollup.config.js
@@ -12,64 +12,49 @@ import copy from 'rollup-plugin-copy';
  * @param {*} source The base folder to start serching
  * @returns a list of file paths that indicate the exported web component
  */
+
 const getPrimaryComponent = source =>
     readdirSync(source, { withFileTypes: true })
-    .filter(entry => entry.isDirectory())
-    .map(entry => path.join(source, entry.name, `${entry.name}.ts`))
-    .filter(x => existsSync(x));
+        .filter(entry => entry.isDirectory())
+        .map(entry => path.join(source, entry.name, `${entry.name}.ts`))
+        .filter(x => existsSync(x));
 
 const individualComponents = getPrimaryComponent('./src/components');
 const additionalFiles = []
-    // const additionalFiles = ['./src/index.ts']
 
-export default [{
-    input: [...additionalFiles, ...individualComponents],
-    output: {
-        format: 'es',
-        chunkFileNames: '[name]-[hash].js',
-        entryFileNames: '[name].js',
-        dir: './dist',
-        sourcemap: true,
+export default [
+    {
+        input: [...additionalFiles, ...individualComponents],
+        output: {
+            format: 'es',
+            chunkFileNames: '[name]-[hash].js',
+            entryFileNames: '[name].js',
+            dir: './dist',
+            sourcemap: true,
+        },
+
+        plugins: [
+            cleaner({ targets: ['./dist/'] }),
+            resolve({ extensions: ['.ts', '.js'] }),
+            commonjs({ include: ['node_modules/**'] }),
+            babel({
+                babelHelpers: 'bundled', extensions: ['.ts'],
+                assumptions: { setPublicClassFields: true },
+                presets: [
+                    [
+                        '@babel/preset-env', {
+                            shippedProposals: true,
+                            bugfixes: true,
+                        },
+                    ],
+                    '@babel/preset-typescript',
+                ],
+                plugins: [
+                    ['@babel/plugin-proposal-decorators', { decoratorsBeforeExport: true }],
+                    ['@babel/plugin-proposal-class-properties'],
+                ],
+            }),
+            copy({ targets: [{ src: 'src/components/**/*.js', dest: 'dist' }] }),
+        ],
     },
-
-    plugins: [
-        cleaner({
-            targets: [
-                './dist/'
-            ]
-        }),
-        resolve({
-            extensions: ['.ts', '.js'],
-        }),
-        commonjs({
-            include: ['node_modules/**'],
-        }),
-        babel({
-            babelHelpers: 'bundled',
-            extensions: ['.ts'],
-            assumptions: { setPublicClassFields: true },
-            presets: [
-                [
-                    '@babel/preset-env',
-                    {
-                        shippedProposals: true,
-                        bugfixes: true,
-                    },
-                ],
-                '@babel/preset-typescript',
-            ],
-            plugins: [
-                [
-                    '@babel/plugin-proposal-decorators',
-                    { decoratorsBeforeExport: true },
-                ],
-                ['@babel/plugin-proposal-class-properties'],
-            ],
-        }),
-        copy({
-            targets: [
-                { src: 'src/components/**/*.js', dest: 'dist' }
-            ]
-        })
-    ],
-}, ];
+];
